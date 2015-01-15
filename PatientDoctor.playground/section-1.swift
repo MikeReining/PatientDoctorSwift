802d4b8a949088cb9cbd0b054bbf4cfc86d6658c
Mike Reining <emailmike@gmail.com>
2015-01-15 10:32:06 -0800

    Task 1 Completed

---
+++ PatientDoctor.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+// MARK: Task 1 - General Setup
+
+class Patient {
+    var name: String
+    var age: Int
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+}
+
+
+
+struct Doctor {
+    var name: String
+    var specialization: String
+}
+
---
+++ PatientDoctor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PatientDoctor
+//
+//  Created by Michael Reining on 1/15/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
